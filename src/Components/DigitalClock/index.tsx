9b6253104929f011da6d87f41b43ccb48a7ca615
Gaurav Deshwal <gauravd@geekyants.com>
2023-11-03 12:49:29 +0530

    fixes in digital clock

--- src/Components/DigitalClock/index.tsx
+++ src/Components/DigitalClock/index.tsx
@@ -20,8 +20,8 @@ const DigitalClock = () => {
       clearInterval(interval);
     };
   }, []);
-
-  const hours = String(currentTime.getHours() % 12).padStart(2, "0");
+  const currentHour = currentTime.getHours();
+  const hours = (currentHour % 12 || 12).toString().padStart(2, "0");
   const minutes = String(currentTime.getMinutes()).padStart(2, "0");
   const seconds = String(currentTime.getSeconds()).padStart(2, "0");
 
